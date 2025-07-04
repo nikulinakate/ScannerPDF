60df68971e7fce2baa574d1951148062bdf62fa2
Ekaterina Nikulina <nikulinakateapp@gmail.com>
2025-07-04 22:31:52 +0500

    Initial Commit

---
+++ Scanner PDF/Scanner_PDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scanner_PDFApp.swift
+//  Scanner PDF
+//
+//  Created by Nikulina Ekaterina on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Scanner_PDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
